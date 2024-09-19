a6bcf601a7735242a36d63f99da06a24f69510fe
rehansumra <rsumra207@gmail.com>
2024-09-19 09:39:28 +0530

    P6
    FIBONACCI

---
+++ Fibonacci.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1=0,n2=2,i,n3;
+    // cout << n2<< endl;
+
+    for(i=0; i<=10; i++)
+
+    {
+        n3=n1+n2;
+        n1=n2;
+        n2=n3;
+        cout << n3 << endl;
+    }
+    return 0;
+}
